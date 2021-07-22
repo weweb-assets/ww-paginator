4865f6e9261589b2430714860346967a5706fa42
Aurélie VIOLETTE <aurelie.violette@purple-fox.fr>
2021-07-22 15:22:41 +0200

    Paginator

---
+++ README.md
@@ -0,0 +1,15 @@
+# WW-PAGINATOR
+
+This is an element for [weweb.io](https://www.weweb.io/).
+
+## Installation
+
+To run locally, first install all dependencies with `npm install`.
+
+## Start
+
+To serve locally, run `npm run serve --port=[PORT]`, and then go to Weweb editor, open developper popup and add localhost:[PORT] as custom element.
+
+## Build
+
+Before release, you can check build error by running `npm run build --name="ww-paginator" --type="element"`
---
+++ src/wwElement.vue
@@ -0,0 +1,142 @@
+<template>
+    <nav role="navigation">
+        <ul>
+            <li :class="{ 'hide-icon': !isEditing && currentPage === 0 }" @click="prev">
+                <wwObject v-if="content.paginatorPrev" v-bind="content.paginatorPrev"></wwObject>
+            </li>
+            <template v-if="content.paginatorText">
+                <li
+                    v-for="(nav, index) in navigation"
+                    :key="index"
+                    :aria-current="nav.index === currentPage"
+                    @click="goTo(nav.index)"
+                >
+                    <wwObject
+                        v-bind="content.paginatorText"
+                        :ww-props="{ text: nav.label }"
+                        :states="nav.states"
+                    ></wwObject>
+                </li>
+            </template>
+            <li :class="{ 'hide-icon': !isEditing && currentPage === nbPage - 1 }" @click="next">
+                <wwObject v-if="content.paginatorNext" v-bind="content.paginatorNext"></wwObject>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    wwDefaultContent: {
+        collectionId: null,
+        paginatorText: wwLib.element('ww-text'),
+        paginatorPrev: wwLib.element('ww-icon', { content: { icon: 'fas fa-angle-left' } }),
+        paginatorNext: wwLib.element('ww-icon', { content: { icon: 'fas fa-angle-right' } }),
+    },
+    props: {
+        content: { type: Object, required: true },
+        /* wwEditor:start */
+        wwEditorState: { type: Object, required: true },
+        /* wwEditor:end */
+    },
+    computed: {
+        isEditing() {
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
+        paginationOptions() {
+            if (!this.content.collectionId) return null;
+            return wwLib.getPaginationOptions(this.content.collectionId);
+        },
+        nbPage() {
+            if (!this.paginationOptions) return 10;
+            return Math.ceil(this.paginationOptions.total / this.paginationOptions.limit);
+        },
+        currentPage() {
+            if (!this.paginationOptions) return 1;
+            return Math.floor(this.paginationOptions.offset / this.paginationOptions.limit);
+        },
+        navigation() {
+            const lastPage = this.nbPage - 1;
+            const prev = this.currentPage - 1;
+            const next = this.currentPage + 1;
+            let index = 0;
+            let navigation = [];
+            navigation.push({ label: '1', index: 0 });
+
+            // Prev page
+            if (prev > index) {
+                // Separator
+                if (prev > index + 1) {
+                    navigation.push({ label: '...', index: -1 });
+                }
+                navigation.push({ label: `${prev + 1}`, index: prev });
+                index = prev;
+            }
+
+            // Current page
+            if (this.currentPage !== 0 && this.currentPage !== lastPage) {
+                navigation.push({ label: `${this.currentPage + 1}`, index: this.currentPage });
+                index = this.currentPage;
+            }
+
+            // NextPage
+            if (next < lastPage && next > index) {
+                navigation.push({ label: `${next + 1}`, index: next });
+                index = next;
+                // Separator
+                if (next < lastPage - 1) {
+                    navigation.push({ label: '...', index: -1 });
+                }
+            }
+
+            // Last page
+            if (lastPage > index) {
+                navigation.push({ label: `${lastPage + 1}`, index: lastPage });
+            }
+
+            return navigation;
+        },
+    },
+    methods: {
+        goTo(index) {
+            if (!this.paginationOptions) return;
+            if (index !== -1 && index !== this.currentPage) {
+                wwLib.setOffset(this.collectionId, index * this.paginationOptions.limit);
+            }
+        },
+        prev() {
+            if (this.currentPage > 0) {
+                this.goTo(this.currentPage - 1);
+            }
+        },
+        next() {
+            if (this.currentPage < this.nbPage - 1) {
+                this.goTo(this.currentPage + 1);
+            }
+        },
+    }
+};
+</script>
+
+ <style lang="scss" scoped>
+ul {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    list-style: none;
+    margin: 0;
+    li {
+        padding: 0;
+        user-select: none;
+        cursor: pointer;
+    }
+}
+.hide-icon {
+    opacity: 0;
+    pointer-events: none;
+}
+</style>
